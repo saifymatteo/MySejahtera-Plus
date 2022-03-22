c1f171b923fb77121364d4153b3f14fc8ecf1cd7
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-03-22 23:38:37 +0800

    testing with bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:mysejahtera_plus/cubit/counter_cubit.dart';
+import 'screens/counter_screen.dart';
+import 'helper/screen_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +15,44 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'MySejahtera Plus',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme:
+                GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)),
+        routes: {
+          Screens.home: (context) => const ChooseScreen(),
+          Screens.counterScreen: (context) => const CounterScreen(),
+        },
       ),
-      home: null,
+    );
+  }
+}
+
+class ChooseScreen extends StatelessWidget {
+  const ChooseScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
+            onPressed: () =>
+                Navigator.pushNamed(context, Screens.counterScreen),
+            child: const Text('Counter Screen'),
+          ),
+          const ElevatedButton(
+            onPressed: null,
+            child: Text('null'),
+          )
+        ],
+      ),
     );
   }
 }
