6142c54b20ad2e7b0aa0657e5686c419a466f78c
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-03-23 23:51:27 +0800

    added Bottom Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
-import 'package:mysejahtera_plus/cubit/counter_cubit.dart';
-import 'screens/counter_screen.dart';
-import 'helper/screen_routes.dart';
+import 'helper/constant.dart';
+
+import 'screens/home_screen.dart';
+import 'screens/statistic_screen.dart';
+import 'screens/check_in_screen.dart';
+import 'screens/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,44 +18,63 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterCubit>(
-      create: (context) => CounterCubit(),
-      child: MaterialApp(
-        title: 'MySejahtera Plus',
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-            textTheme:
-                GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)),
-        routes: {
-          Screens.home: (context) => const ChooseScreen(),
-          Screens.counterScreen: (context) => const CounterScreen(),
-        },
-      ),
+    return MaterialApp(
+      title: 'MySejahtera Plus',
+      theme: ThemeData(
+          primarySwatch: kPrimarySwatch,
+          textTheme:
+              GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)),
+      home: const MainComponent(),
     );
   }
 }
 
-class ChooseScreen extends StatelessWidget {
-  const ChooseScreen({Key? key}) : super(key: key);
+class MainComponent extends StatefulWidget {
+  const MainComponent({Key? key}) : super(key: key);
+
+  @override
+  State<MainComponent> createState() => _MainComponentState();
+}
+
+class _MainComponentState extends State<MainComponent> {
+  int screenIndex = 2;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          ElevatedButton(
-            onPressed: () =>
-                Navigator.pushNamed(context, Screens.counterScreen),
-            child: const Text('Counter Screen'),
-          ),
-          const ElevatedButton(
-            onPressed: null,
-            child: Text('null'),
-          )
-        ],
+      body: screenOptions.elementAt(screenIndex),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SalomonBottomBar(
+          currentIndex: screenIndex,
+          onTap: (index) => setState(() => screenIndex = index),
+          items: [
+            SalomonBottomBarItem(
+              icon: const Icon(Icons.home),
+              title: const Text('Home'),
+            ),
+            SalomonBottomBarItem(
+              icon: const Icon(Icons.assessment),
+              title: const Text('Statistics'),
+            ),
+            SalomonBottomBarItem(
+              icon: const Icon(Icons.qr_code),
+              title: const Text('Check-In'),
+            ),
+            SalomonBottomBarItem(
+              icon: const Icon(Icons.person),
+              title: const Text('Profile'),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+List<Widget> screenOptions = <Widget>[
+  const HomeScreen(),
+  const StatisticScreen(),
+  const CheckInScreen(),
+  const ProfileScreen(),
+];
