c5847f00713d47ad53b0bf132b993b3178fe99fc
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-03-26 23:33:11 +0800

    added new colors

--- lib/helper/constant.dart
+++ lib/helper/constant.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
+
 import 'material_color_converter.dart';
 
-MaterialColor kPrimarySwatch = createMaterialColor(const Color(0xFF4a5bf1));
+Color kWhiteColor = const Color(0xFFFFFFFF);
+Color kDarkWhiteColor = const Color(0xFFf3f4f9);
+Color kDarkGreyColor = const Color(0xFF2C2C2C);
+Color kPrimarySwatch = const Color(0xFF4a5bf1);
+
+Color kVaccinatedFull = const Color(0xFF59bc98);
+Color kVaccinatedFullDark = const Color(0xFF14523B);
+Color kVaccinatedPartial = const Color(0xFFe1b84d);
+Color kVaccinatedNot = const Color(0xFFdc5d5d);
+
+Color kRiskStatusGreen = const Color(0xFF6ad782);
+Color kRiskStatusRed = const Color(0xFFdc5d5d);
+
+MaterialColor kPrimarySwatchTheme =
+    createMaterialColor(const Color(0xFF4a5bf1));
