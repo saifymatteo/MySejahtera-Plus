cd1fb9cc87cbdc61a52d08b1e77d99001d1557c3
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-04-23 23:24:26 +0800

    added FutureBuilder

---
+++ lib/data_layers/repositories/statistics_malaysia_repositories.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:intl/intl.dart';
+
+import '../../helper/statistic_calculator.dart';
+import '../models/statistics_malaysia_model.dart';
+
+const urlUpdateCases =
+    'https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/cases_malaysia.csv';
+const urlUpdateDeath =
+    'https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/deaths_malaysia.csv';
+
+DefaultCacheManager cacheManager = DefaultCacheManager();
+final numberFormatter = NumberFormat('#,##0', 'en_US');
+
+class StatisticsDataMalaysiaRepositories {
+  List<StatisticsDataMalaysiaModel> data = [];
+
+  Future<List<StatisticsDataMalaysiaModel>> fetchMalaysiaData() async {
+    try {
+      // Convert the CSV file to List<List<dynamic>>
+      final casesData =
+          await getDataCSV(cacheManager: cacheManager, urlCSV: urlUpdateCases);
+      final deathData =
+          await getDataCSV(cacheManager: cacheManager, urlCSV: urlUpdateDeath);
+
+      // Get to the latest row of the CSV
+      final cases2ndLastField = casesData[casesData.length - 2];
+      final casesLastField = casesData.last;
+      final death2ndLastField = deathData[deathData.length - 2];
+      final deathLastField = deathData.last;
+
+      debugPrint('Covid-19 Malaysia Data is being updated');
+
+      List dataList = [];
+      List differences = [];
+
+      //* Confirmed Card data
+      dataList.add(numberFormatter
+          .format(columnCalculate(wholeFields: casesData, columnIndex: 1)));
+      dataList.add(numberFormatter.format(casesLastField.elementAt(1)));
+
+      differences = calculateDifference(
+          today: casesLastField, yesterday: cases2ndLastField, index: 1);
+
+      dataList.add(differences[0].toString());
+      dataList.add(differences[1]);
+      dataList.add(differences[2]);
+
+      data.add(StatisticsDataMalaysiaModel.fromCsv(dataList));
+      dataList.clear();
+      differences.clear();
+
+      //* Recovered Card data
+      dataList.add(numberFormatter
+          .format(columnCalculate(wholeFields: casesData, columnIndex: 3)));
+      dataList.add(numberFormatter.format(casesLastField.elementAt(3)));
+
+      differences = calculateDifference(
+          today: casesLastField, yesterday: cases2ndLastField, index: 3);
+
+      dataList.add(differences[0].toString());
+      dataList.add(differences[1]);
+      dataList.add(differences[2]);
+
+      data.add(StatisticsDataMalaysiaModel.fromCsv(dataList));
+      dataList.clear();
+      differences.clear();
+
+      //* Death Card data
+      dataList.add(numberFormatter
+          .format(columnCalculate(wholeFields: deathData, columnIndex: 1)));
+      dataList.add(numberFormatter.format(deathLastField.elementAt(1)));
+
+      differences = calculateDifference(
+          today: deathLastField, yesterday: death2ndLastField, index: 1);
+
+      dataList.add(differences[0].toString());
+      dataList.add(differences[1]);
+      dataList.add(differences[2]);
+
+      data.add(StatisticsDataMalaysiaModel.fromCsv(dataList));
+      dataList.clear();
+      differences.clear();
+
+      //* Active Card data
+      dataList.add(numberFormatter.format(casesLastField.elementAt(4)));
+      dataList.add(numberFormatter.format(casesLastField.elementAt(1)));
+
+      differences = calculateDifference(
+          today: casesLastField, yesterday: cases2ndLastField, index: 4);
+
+      dataList.add(differences[0].toString());
+      dataList.add(differences[1]);
+      dataList.add(differences[2]);
+
+      data.add(StatisticsDataMalaysiaModel.fromCsv(dataList));
+      dataList.clear();
+      differences.clear();
+
+      return data;
+    } catch (e) {
+      debugPrint('Error: $e');
+      throw Exception('Covid-19 Global Data could not be updated');
+    }
+  }
+}
--- lib/presentation/components/grid_statistics_updates.dart
+++ lib/presentation/components/grid_statistics_updates.dart
@@ -1,76 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:mysejahtera_plus/data_layers/repositories/statistics_malaysia_repositories.dart';
 
-import '../../data_layers/models/statistics_model.dart';
 import '../../helper/constant.dart';
 import 'card_statistics.dart';
 
-class GridStatisticsUpdates extends StatefulWidget {
-  const GridStatisticsUpdates({
-    Key? key,
-    required this.time,
-  }) : super(key: key);
+class GridStatisticsUpdates extends StatelessWidget {
+  GridStatisticsUpdates({Key? key, required this.time}) : super(key: key);
 
+  final StatisticsDataMalaysiaRepositories data =
+      StatisticsDataMalaysiaRepositories();
   final String time;
 
   @override
-  State<GridStatisticsUpdates> createState() => _GridStatisticsUpdatesState();
-}
-
-class _GridStatisticsUpdatesState extends State<GridStatisticsUpdates> {
-  StatisticsDataUpdates data = StatisticsDataUpdates();
-
-  // TODO: Use FutureBuilder for the data
-
-  @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      mainAxisSpacing: 5,
-      crossAxisSpacing: 5,
-      crossAxisCount: 2,
-      childAspectRatio: 4 / 4,
-      children: [
-        CardStatistics(
-          cardColor: kStatisticRed,
-          header: 'Total Confirmed Cases',
-          totalText: data.caseConfirmed,
-          plusOrMinus: data.caseConfirmedPlus,
-          numberTodayText: data.caseConfirmedToday,
-          iconArrow: data.caseConfirmedArrow,
-          percentage: data.caseConfirmedPercent,
-          date: widget.time,
-        ),
-        CardStatistics(
-          cardColor: kStatisticGreen,
-          header: 'Total Recovered',
-          totalText: data.caseRecovered,
-          plusOrMinus: data.caseRecoveredPlus,
-          numberTodayText: data.caseRecoveredToday,
-          iconArrow: data.caseRecoveredArrow,
-          percentage: data.caseRecoveredPercent,
-          date: widget.time,
-        ),
-        CardStatistics(
-          cardColor: kStatisticGrey,
-          header: widget.time,
-          totalText: data.caseDeath,
-          plusOrMinus: data.caseDeathPlus,
-          numberTodayText: data.caseDeathToday,
-          iconArrow: data.caseDeathArrow,
-          percentage: data.caseDeathPercent,
-          date: widget.time,
-        ),
-        CardStatistics(
-          cardColor: kStatisticOrange,
-          header: widget.time,
-          totalText: data.caseActive,
-          plusOrMinus: data.caseActivePlus,
-          numberTodayText: data.caseActiveToday,
-          iconArrow: data.caseActiveArrow,
-          percentage: data.caseActivePercent,
-          date: widget.time,
-        ),
-        const SizedBox(height: 10),
-      ],
+    return FutureBuilder(
+      future: data.fetchMalaysiaData(),
+      builder: (context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return GridView.count(
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5,
+            crossAxisCount: 2,
+            childAspectRatio: 4 / 4,
+            children: [
+              CardStatistics(
+                cardColor: kStatisticRed,
+                header: 'Total Confirmed Cases',
+                totalText: snapshot.data![0].cardConfirmedModel.caseConfirmed,
+                plusOrMinus:
+                    snapshot.data![0].cardConfirmedModel.caseConfirmedPlus,
+                numberTodayText:
+                    snapshot.data![0].cardConfirmedModel.caseConfirmedToday,
+                iconArrow:
+                    snapshot.data![0].cardConfirmedModel.caseConfirmedArrow,
+                percentage:
+                    snapshot.data![0].cardConfirmedModel.caseConfirmedPercent,
+                date: time,
+              ),
+              CardStatistics(
+                cardColor: kStatisticGreen,
+                header: 'Total Recovered',
+                totalText: snapshot.data![1].cardRecoveredModel.caseRecovered,
+                plusOrMinus:
+                    snapshot.data![1].cardRecoveredModel.caseRecoveredPlus,
+                numberTodayText:
+                    snapshot.data![1].cardRecoveredModel.caseRecoveredToday,
+                iconArrow:
+                    snapshot.data![1].cardRecoveredModel.caseRecoveredArrow,
+                percentage:
+                    snapshot.data![1].cardRecoveredModel.caseRecoveredPercent,
+                date: time,
+              ),
+              CardStatistics(
+                cardColor: kStatisticGrey,
+                header: 'Total Death',
+                totalText: snapshot.data![2].cardDeathModel.caseDeath,
+                plusOrMinus: snapshot.data![2].cardDeathModel.caseDeathPlus,
+                numberTodayText:
+                    snapshot.data![2].cardDeathModel.caseDeathToday,
+                iconArrow: snapshot.data![2].cardDeathModel.caseDeathArrow,
+                percentage: snapshot.data![2].cardDeathModel.caseDeathPercent,
+                date: time,
+              ),
+              CardStatistics(
+                cardColor: kStatisticOrange,
+                header: 'Total Active Cases',
+                totalText: snapshot.data![3].cardActiveModel.caseActive,
+                plusOrMinus: snapshot.data![3].cardActiveModel.caseActivePlus,
+                numberTodayText:
+                    snapshot.data![3].cardActiveModel.caseActiveToday,
+                iconArrow: snapshot.data![3].cardActiveModel.caseActiveArrow,
+                percentage: snapshot.data![3].cardActiveModel.caseActivePercent,
+                date: time,
+              ),
+              const SizedBox(height: 10),
+            ],
+          );
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+            padding: const EdgeInsets.all(30),
+            width: 50,
+            height: 50,
+            child: const CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return const Text('Error, try to restart the app');
+        }
+      },
     );
   }
 }
