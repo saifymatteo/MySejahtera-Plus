0498e34d16840c8c7dedef88c376f11781ed1595
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-04-23 23:22:25 +0800

    added KeepAlivePage for PageView widget

---
+++ lib/helper/pageview_keepalive.dart
@@ -0,0 +1,28 @@
+// Credit goes to O Thạnh Ldt
+// https://stackoverflow.com/a/63574708/16060347
+
+import 'package:flutter/material.dart';
+
+class KeepAlivePage extends StatefulWidget {
+  const KeepAlivePage({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  _KeepAlivePageState createState() => _KeepAlivePageState();
+}
+
+class _KeepAlivePageState extends State<KeepAlivePage>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return widget.child;
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
